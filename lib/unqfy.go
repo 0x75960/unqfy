da47376a72f0feb91528757312f50cb10445e8bb
Hiromitsu Oshiba <h.oshiba.0117@gmail.com>
2017-05-20 01:09:18 +0900

    initial commit.

---
+++ lib/unqfy.go
@@ -0,0 +1,167 @@
+package unqfy
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+//Listup file in directory
+func Listup(root string) ([]string, error) {
+
+	pathlist := []string{}
+
+	err := filepath.Walk(root,
+		func(path string, info os.FileInfo, err error) error {
+
+			if info.IsDir() {
+				return nil
+			}
+
+			rel, err := filepath.Rel(root, path)
+
+			pathlist = append(pathlist, rel)
+
+			return nil
+		})
+
+	if err != nil {
+		return pathlist, err
+	}
+
+	return pathlist, nil
+}
+
+// ListupInDirs specified
+func ListupInDirs(dirs []string) (all []string, err error) {
+
+	for _, dir := range dirs {
+
+		files, err := Listup(dir)
+		if err != nil {
+			return all, err
+		}
+
+		for _, file := range files {
+			all = append(all, file)
+		}
+
+	}
+
+	return
+
+}
+
+// Uniqify files
+func Uniqify(files []string) (uniqified []string, err error) {
+
+	ret := make(map[string][]string)
+
+	for _, file := range files {
+
+		sum, err := sha256sum(file)
+		if err != nil {
+			return uniqified, err
+		}
+
+		// sha256でまとめる
+		ret[sum] = append(ret[sum], file)
+	}
+
+	for _, value := range ret {
+		uniqified = append(uniqified, value[0])
+	}
+
+	return
+}
+
+// sha256sum of file
+func sha256sum(file string) (sum string, err error) {
+
+	sha256er := sha256.New()
+
+	f, err := os.Open(file)
+	if err != nil {
+		return sum, err
+	}
+	defer f.Close()
+
+	_, err = io.Copy(sha256er, f)
+	if err != nil {
+		return sum, err
+	}
+
+	return hex.EncodeToString(sha256er.Sum(nil)), nil
+}
+
+// Copy unique files in directory.
+func Copy(dst, src string) (err error) {
+
+	// フォルダの中のファイルを列挙
+	all, err := Listup(src)
+	if err != nil {
+		return err
+	}
+
+	// 見つけたファイルをユニークに
+	uniqified, err := Uniqify(all)
+	if err != nil {
+		return err
+	}
+
+	// 出力先ディレクトリが存在しない場合作成
+	_, err = os.Stat(dst)
+	if err != nil {
+
+		err = os.MkdirAll(dst, os.ModePerm)
+		if err != nil {
+			return err
+		}
+
+		log.Printf("%s not found. created.", dst)
+
+	}
+
+	for _, file := range uniqified {
+
+		sum, err := sha256sum(file)
+		if err != nil {
+			return err
+		}
+
+		// 出力先ディレクトリにSHA256のファイル名でコピー
+		dstPath := path.Join(dst, sum)
+		err = filecopy(dstPath, file)
+		if err != nil {
+			return err
+		}
+
+	}
+
+	return
+
+}
+
+// filecopy to dst from src
+func filecopy(dst, src string) (err error) {
+
+	d, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+
+	s, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	_, err = io.Copy(d, s)
+
+	return
+}
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	unqfy "github.com/0x75960/unqfy/lib"
+)
+
+var srcdir string
+var dstdir string
+
+func init() {
+
+	if len(os.Args) != 3 {
+		fmt.Printf("\nusage:\n\t%s src_dir dest_dir\n\n", os.Args[0])
+		os.Exit(-1)
+	}
+
+	srcdir = os.Args[1]
+	dstdir = os.Args[2]
+
+}
+
+func main() {
+
+	err := unqfy.Copy(dstdir, srcdir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
